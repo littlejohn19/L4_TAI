1b0c713e811a62f1ebfd4ed4f71dbed0aa26a4a3
Adsel <marcin.radwan08@gmail.com>
2021-03-20 19:24:50 +0100

    feat: results from localstorage

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ const steps = document.querySelector('.steps');
 const results = document.querySelector('.results');
 const pointsElem = document.querySelector('.score');
 const userScorePoint = document.querySelector('.userScorePoint');
+const userScorePercent = document.querySelector('.userScorePercent');
+const avgScorePoint = document.querySelector('.avgScorePoint');
+const avgScorePercent = document.querySelector('.avgScorePercent');
 
 let index = 0;
 let points = 0;
@@ -75,7 +78,6 @@ function nextQuestion() {
 function saveAndShowResults() {
     list.style.display = 'none';
     results.style.display = 'block';
-    userScorePoint.innerHTML = points;
 
     const lastGamesCounter = localStorage.getItem(countOfGamesLSKey);
     const gamesCounter = lastGamesCounter != null ? parseInt(lastGamesCounter) + 1: 1;
@@ -84,11 +86,24 @@ function saveAndShowResults() {
     const lastAvg = localStorage.getItem(quizAverageLSKey);
     const avg = lastAvg != null ? (parseFloat(lastAvg) + points) / 2 : points;
     localStorage.setItem(quizAverageLSKey, avg);
+
+    showPoints();
+}
+
+function showPoints() {
+    const maxPoints = preQuestions.length;
+    const placesAfterPoint = 2;
+
+    userScorePoint.innerHTML = points;
+    userScorePercent.innerHTML = String(points * 100 / maxPoints);
+
+    const avgScores = localStorage.getItem(quizAverageLSKey);
+    avgScorePoint.innerHTML = String(parseFloat(avgScores).toFixed(placesAfterPoint));
+    avgScorePercent.innerHTML = String((parseFloat(avgScores) * 100 / maxPoints).toFixed(placesAfterPoint));
 }
 
 function restartQuiz() {
-    index = 0;
-    points = 0;
+    index = points = 0;
     const userScorePoint = document.querySelector('.score');
     steps.innerHTML = '';
     createSteps();
